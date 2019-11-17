42f45e098ab6512c473fd0782c6ffa9f8be6ec52
Artsem <gorbatchevartsem@gmail.com>
2019-11-17 21:49:58 +0300

    slight change in SceneLoader (now not saved after switching to another page) and ResultsSave. add MainMenu and Results Scene

--- Assets/Scripts/ResultsSave.cs
+++ Assets/Scripts/ResultsSave.cs
@@ -10,10 +10,6 @@ public static class ResultsSave
     {
         get { return questionsNumber; }
     }
-    public static void DecrementQuestionsNumber()
-    {
-        questionsNumber--;
-    }
 
     //answers
     private static int rightAnswersNumber;
@@ -21,15 +17,48 @@ public static class ResultsSave
     {
         get { return rightAnswersNumber; }
     }
+
+    //other
+    public const int SCENE_START = 1; // минимальный индекс сцены
+    public const int SCENE_FINISH = 1; // финальная сцена, где пишется, что игра окончена
+
+    private static List<int> sceneIndexes = new List<int>(); // список сцен
+    private static System.Random random = new System.Random(); // генератор случайных чисел
+    private static int curSceneIndex = SCENE_START; // приватное поле с текущим количеством сцен
+
+    //methods
+    public static void UpdateParameters(int numberOfQuestions)
+    {
+        questionsNumber = numberOfQuestions;
+        rightAnswersNumber = 0;
+
+        sceneIndexes.Clear();
+        for (int i = SCENE_START; i < SCENE_FINISH; i++)
+            sceneIndexes.Add(i);
+    }
+
+    public static void DecrementQuestionsNumber()
+    {
+        questionsNumber--;
+    }
+
     public static void IncrementRightAnswersNumber()
     {
         rightAnswersNumber++;
     }
 
-    //other
-    public static void UpdateParameters(int numberOfQuestions)
+    // выбираем рандомный индекс сцены которая не была разыграна
+    public static int RandomSceneIndex()
     {
-        questionsNumber = numberOfQuestions;
-        rightAnswersNumber = 0;
+        int tmp = -1;
+
+        if (sceneIndexes.Count != 0)
+        {
+            int randomIndex = random.Next(0, sceneIndexes.Count);
+            tmp = sceneIndexes[randomIndex];
+            sceneIndexes.RemoveAt(randomIndex);
+        }
+
+        return tmp;
     }
 }
