f99b6f4c0ad344a3d5527a7c8a74651dc32a45ad
Artsem <gorbatchevartsem@gmail.com>
2019-11-22 21:14:44 +0300

    Fix problem with background and add random for background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public GameObject background;
+    public Sprite[] backgroundSprites;
+
+    private SpriteRenderer backgroundSpriteRenderer;
+
+    private void Start()
+    {
+        backgroundSpriteRenderer = background.GetComponent<SpriteRenderer>();
+
+        SetRandomBackground(backgroundSprites);
+    }
+
+    private void SetRandomBackground(params Sprite[] sprites)
+    {
+        if(sprites != null)
+        {
+            int randomSpriteIndex = Random.Range(0, sprites.Length);
+            backgroundSpriteRenderer.sprite = sprites[randomSpriteIndex];
+        }
+    }
+}
