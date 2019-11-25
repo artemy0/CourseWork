ff1e9c158cfa20a6380872b03609f1e6ff5c3ced
Artsem <gorbatchevartsem@gmail.com>
2019-11-25 22:28:24 +0300

    Add opaque and transparent anim for indefObj and Grid, change getResultButton(or other name?!)

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animation anim;
+
+    public void PlayAnim()
+    {
+        anim.Play();
+    }
+
+    public void PlayChildAnim()
+    {
+        Animation[] childAnims = this.GetComponentsInChildren<Animation>();
+
+        for (int i = 1; i < childAnims.Length; i++)
+        {
+            childAnims[i].Play();
+        }
+    }
+}
