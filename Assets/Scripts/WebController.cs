dfd8420cf2de65c8048a0f0737834b4539bb9dbc
Artsem <gorbatchevartsem@gmail.com>
2019-11-30 00:28:17 +0300

    Change canves logic, add webSiteButtin for open web site and add easy mibile for future developments

---
+++ Assets/Scripts/WebController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebController : MonoBehaviour
+{
+    public void OpenWebSite(string webPageAddress)
+    {
+        if (webPageAddress != null)
+            Application.OpenURL(webPageAddress);
+    }
+}
